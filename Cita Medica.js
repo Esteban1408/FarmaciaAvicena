c1ccb3a736132e27ab59cd6f03b1d7f2ae754904
Esteban1408 <71802180+Esteban1408@users.noreply.github.com>
2020-09-23 21:29:06 -0500

    Add files via upload

---
+++ Cita Medica.js
@@ -0,0 +1,10 @@
+function init(){
+	document.write("hola mundo")
+	var numero=11;
+	return window.location="intento.html";
+}
+function init2(){
+	var numero=12;
+	document.write("hola mundo x2")
+	return window.location="intento.html";
+}
